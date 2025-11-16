3b341a2525f7df0947807e10f11d2b977517527e
Amr mousa <amarmousa1223@gmail.com>
2025-11-17 01:26:11 +0200

    exec stored procrdure

--- Program.cs
+++ Program.cs
@@ -28,10 +28,10 @@ namespace Practice_On_EFCore
 
             var student = new Student { Name = "Ali", Age = 23 };
             student.Courses = new List<Course>
-            {
-                new Course { Title = "Math" },
-                new Course { Title = "Physics" }
-            };
+              {
+                  new Course { Title = "Math" },
+                  new Course { Title = "Physics" }
+              };
             context.Students.Add(student);
             context.SaveChanges();
 
@@ -49,14 +49,14 @@ namespace Practice_On_EFCore
                 }
             }
 
-           // CRUD
-             //Insert
+            // CRUD
+            //Insert
             var newStudent = new Student
             {
                 Age = 34,
                 Name = "Hisham Selim",
                 Courses = { new Course { Title = ".Net" },
-                    new Course { Title = "PHP" } }
+                      new Course { Title = "PHP" } }
             };
             context.Students.Add(newStudent);
 
@@ -71,18 +71,18 @@ namespace Practice_On_EFCore
             context.Students.Remove(deleteStudent);
             context.SaveChanges();
 
-           // GetById
+            // GetById
             var getStdById = context.Students.Find(1);
             Console.WriteLine($"StdId:{getStdById.Id},StdName:{getStdById.Name} ,SdtAge:{getStdById.Age}");
 
 
-           // GetAll
-           var allStudents = context.Students.ToList();
+            // GetAll
+            var allStudents = context.Students.ToList();
 
-          //  Filter
-           var filtered = context.Students.Where(s => s.Age > 20).ToList();
+            //  Filter
+            var filtered = context.Students.Where(s => s.Age > 20).ToList();
 
-          ///  Sorting & Filtering
+            ///  Sorting & Filtering
             var StdFilter = context.Students
                 .Where(x => x.Age > 23)
                 .OrderByDescending(x => x.Age)
@@ -96,11 +96,11 @@ namespace Practice_On_EFCore
             int page = 2;
             int pageSize = 3;
             var paging = context.Students
-                .Skip((page-1) * pageSize)
+                .Skip((page - 1) * pageSize)
                 .Take(pageSize)
                 .ToList();
 
-            foreach ( var p in paging )
+            foreach (var p in paging)
             {
                 Console.WriteLine($"{p.Name} ,{p.Id} ,{p.Age} ");
             }
@@ -110,7 +110,20 @@ namespace Practice_On_EFCore
 
             Console.WriteLine($"Total Pages = {totalPages}");
 
+           // stored Procedure
+            var stds = context.Students
+                .FromSqlRaw("Exec GetStdOverAge {0} ", 20)
+                .ToList();
 
+            foreach (var std in stds)
+            {
+                Console.WriteLine($"StdId:{std.Id}  ,StdName: {std.Name}  ");
+                foreach (var course in std.Courses)
+                {
+                    Console.WriteLine($"CourseName {course.Title}");
+                }
+
+            }
         }
     }
 }
